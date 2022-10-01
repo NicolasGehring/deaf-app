848fdab52b464f8576980cc553e9284c6e67b315
NicolasGehring <n.gehring@online.de>
2022-10-01 10:39:12 +0200

    got drawing working

--- src/Video.js
+++ src/Video.js
@@ -1,59 +1,185 @@
 import React, { useRef, useEffect } from "react"
 import Webcam from "react-webcam"
-import { Holistic } from "@mediapipe/holistic"
+import { Holistic, POSE_LANDMARKS_LEFT,POSE_LANDMARKS_RIGHT,POSE_LANDMARKS,POSE_CONNECTIONS,HAND_CONNECTIONS,  FACEMESH_TESSELATION,FACEMESH_RIGHT_EYE,FACEMESH_RIGHT_EYEBROW,FACEMESH_LEFT_EYE,FACEMESH_LEFT_EYEBROW,FACEMESH_LIPS
+} from "@mediapipe/holistic"
 import { Camera } from "@mediapipe/camera_utils"
+import { drawConnectors,drawLandmarks,lerp } from "@mediapipe/drawing_utils"
 
 export default function Video(props) {
   const faceMeshRef = useRef(null)
   const webcamRef = useRef(null)
   const canvasRef = useRef(null)
-  const filterImgRef = useRef({ current: null })
 
-  function onResults(results) {
-    console.log(results)
-    const videoWidth = webcamRef.current.video.videoWidth
-    const videoHeight = webcamRef.current.video.videoHeight
-
-    canvasRef.current.width = videoWidth
-    canvasRef.current.height = videoHeight
-    const canvasElement = canvasRef.current
-    const canvasCtx = canvasElement.getContext("2d")
-    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height)
-    canvasCtx.drawImage(
-      results.image,
-      0,
-      0,
-      canvasElement.width,
-      canvasElement.height
-    )
-    if (results.multiFaceLandmarks.length > 0) {
-      const keypoints = results.multiFaceLandmarks[0]
-
-      const maskWidth = Math.abs(
-        keypoints[234].x * videoWidth - keypoints[454].x * videoWidth
-      )
-      const maskHeight =
-        Math.abs(
-          keypoints[234].y * videoHeight - keypoints[152].y * videoHeight
-        ) + 10
-      filterImgRef.current.width = `${maskWidth}`
-      filterImgRef.current.height = `${maskHeight}`
-
-      canvasCtx.drawImage(
-        filterImgRef.current,
-        keypoints[234].x * videoWidth,
-        keypoints[234].y * videoHeight - 10,
-        maskWidth,
-        maskHeight
-      )
+  function removeElements(landmarks, elements) {
+    for (const element of elements) {
+        delete landmarks[element];
     }
   }
+  function removeLandmarks(results) {
+      if (results.poseLandmarks) {
+          removeElements(results.poseLandmarks, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 16, 17, 18, 19, 20, 21, 22]);
+      }
+  }
+  function connect(ctx, connectors) {
+      const canvas = ctx.canvas;
+      for (const connector of connectors) {
+          const from = connector[0];
+          const to = connector[1];
+          if (from && to) {
+              if (from.visibility && to.visibility &&
+                  (from.visibility < 0.1 || to.visibility < 0.1)) {
+                  continue;
+              }
+              ctx.beginPath();
+              ctx.moveTo(from.x * canvas.width, from.y * canvas.height);
+              ctx.lineTo(to.x * canvas.width, to.y * canvas.height);
+              ctx.stroke();
+          }
+      }
+  }
+
+let activeEffect = 'mask';
+const COLOR_LEFT = 'rgb(235,105,233)'
+const COLOR_RIGHT = 'rgb(235,105,233)'
+
+function onResults(results) {
+  console.log("results", results)
+  const canvasElement = canvasRef.current
+  const canvasCtx = canvasElement.getContext('2d');
+  console.log(canvasCtx)
+  // Hide the spinner.
+  document.body.classList.add('loaded');
+  // Remove landmarks we don't want to draw.
+  removeLandmarks(results);
+  // Update the frame rate.
+  //fpsControl.tick();
+  // Draw the overlays.
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  if (results.segmentationMask) {
+      canvasCtx.drawImage(results.segmentationMask, 0, 0, canvasElement.width, canvasElement.height);
+      // Only overwrite existing pixels.
+      if (activeEffect === 'mask' || activeEffect === 'both') {
+          canvasCtx.globalCompositeOperation = 'source-in';
+          // This can be a color or a texture or whatever...
+          canvasCtx.fillStyle = '#00FF007F';
+          canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+      }
+      else {
+          canvasCtx.globalCompositeOperation = 'source-out';
+          canvasCtx.fillStyle = '#0000FF7F';
+          canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+      }
+      // Only overwrite missing pixels.
+      canvasCtx.globalCompositeOperation = 'destination-atop';
+      canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+      canvasCtx.globalCompositeOperation = 'source-over';
+  }
+  else {
+      canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+  }
+  // Connect elbows to hands. Do this first so that the other graphics will draw
+  // on top of these marks.
+  canvasCtx.lineWidth = 5;
+  if (results.poseLandmarks) {
+      if (results.rightHandLandmarks) {
+          canvasCtx.strokeStyle = 'white';
+          connect(canvasCtx, [[
+                  results.poseLandmarks[POSE_LANDMARKS.RIGHT_ELBOW],
+                  results.rightHandLandmarks[0]
+              ]]);
+      }
+      if (results.leftHandLandmarks) {
+          canvasCtx.strokeStyle = 'white';
+          connect(canvasCtx, [[
+                  results.poseLandmarks[POSE_LANDMARKS.LEFT_ELBOW],
+                  results.leftHandLandmarks[0]
+              ]]);
+      }
+  }
+  // Pose...
+  
+  if(results.poseLandmarks) {
+    drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, { color: 'white' });
+    drawLandmarks(canvasCtx, Object.values(POSE_LANDMARKS_LEFT)
+        .map(index => results.poseLandmarks[index]), { visibilityMin: 0.65, color: 'white', fillColor: COLOR_LEFT });
+    drawLandmarks(canvasCtx, Object.values(POSE_LANDMARKS_RIGHT)
+        .map(index => results.poseLandmarks[index]), { visibilityMin: 0.65, color: 'white', fillColor: COLOR_RIGHT });
+  }
+  // Hands...
+  if(results.rightHandLandmarks) {
+    drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS, { color: 'white' });
+    drawLandmarks(canvasCtx, results.rightHandLandmarks, {
+        color: 'white',
+        fillColor: COLOR_RIGHT,
+        lineWidth: 2,
+        radius: (data) => {
+            return lerp(data.from.z, -0.15, .1, 10, 1);
+        }
+    });
+  }
+  if(results.leftHandLandmarks) {
+    drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS, { color: 'white' });
+    drawLandmarks(canvasCtx, results.leftHandLandmarks, {
+        color: 'white',
+        fillColor: COLOR_LEFT,
+        lineWidth: 2,
+        radius: (data) => {
+            return lerp(data.from.z, -0.15, .1, 10, 1);
+        }
+    });
+  }
+  // Face...
+  if(results.faceLandmarks) {
+    drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION, { color: '#C0C0C070', lineWidth: 1 });
+    drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_RIGHT_EYE, { color: COLOR_RIGHT });
+    drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_RIGHT_EYEBROW, { color: COLOR_RIGHT });
+    drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_LEFT_EYE, { color: COLOR_LEFT });
+    drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_LEFT_EYEBROW, { color: COLOR_LEFT });
+    //drawConnectors(canvasCtx, results.faceLandmarks, mpHolistic.FACEMESH_FACE_OVAL, { color: '#E0E0E0', lineWidth: 5 });
+    drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_LIPS, { color: COLOR_LEFT, lineWidth: 5 });
+  }
+  // Draw Hand bounding box 
+  let all_coo = []
+  if(results.leftHandLandmarks && results.rightHandLandmarks){
+    all_coo = results.leftHandLandmarks.concat(results.rightHandLandmarks)
+  }else if(results.leftHandLandmarks){
+    all_coo = results.leftHandLandmarks
+  }else if(results.rightHandLandmarks){
+    all_coo = results.rightHandLandmarks
+  }
+  let all_x = []
+  let all_y = []
+  all_coo.forEach(element => {
+    all_x.push(element.x)
+  });
+  all_coo.forEach(element => {
+    all_y.push(element.y)
+  });
+
+  let tl_x = Math.min(...all_x)
+  let tl_y = Math.min(...all_y)
+
+  let br_x = Math.max(...all_x)
+  let br_y = Math.max(...all_y)
+
+  let height= br_y - tl_y
+  let width = br_x - tl_x
+    
+  canvasCtx.strokeRect(
+    tl_x*canvasElement.width,
+    tl_y*canvasElement.height,
+    width*canvasElement.width,
+    height*canvasElement.height);
+
+  canvasCtx.restore();
+}
 
   useEffect(() => {
     console.log("tes")
     const faceMesh = new Holistic({
       locateFile: (file) => {
-        return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic@0.5.1635989137/${file}`
       },
     })
     faceMeshRef.current = faceMesh
@@ -66,20 +192,35 @@ export default function Video(props) {
 
     faceMesh.onResults(onResults)
 
+    const camera = new Camera(webcamRef.current.video, {
+      onFrame: async () => {
+        console.log("sending")
+        webcamRef.current &&
+          (await faceMesh.send({ image: webcamRef.current.video }))
+      },
+      width: 640,
+      height: 480,
+    })
+    camera.start()
+
+    /*
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null
     ) {
+      console.log("test")
       const maskFilterImage = document.createElement("img", {
         ref: filterImgRef,
       })
       maskFilterImage.objectFit = "contain"
       maskFilterImage.onload = function () {
+        console.log("TESt22")
         filterImgRef.current = maskFilterImage
         webcamRef.current.video.crossOrigin = "anonymous"
 
         const camera = new Camera(webcamRef.current.video, {
           onFrame: async () => {
+            console.log("sending")
             webcamRef.current &&
               (await faceMesh.send({ image: webcamRef.current.video }))
           },
@@ -90,7 +231,9 @@ export default function Video(props) {
       }
       maskFilterImage.src = "images/mask.png"
     }
+    */
   }, [])
+
 
   const cleanUpFunc = () => {
     faceMeshRef.current && faceMeshRef.current.close()
